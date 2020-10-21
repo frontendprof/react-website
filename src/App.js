979af858227d34ef7874883de15d47c1fab58fc4
frontendprof <abdumaliksharipov@gmail.com>
2020-10-21 23:59:46 +0500

    WOrking on routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import React from 'react';
+
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from react-router-dom;
+
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World </h1>
-    </div>
-  );
+
+
+class App extends Component {
+
+  render() {
+
+    return (
+      <React.Fragment>
+        <h1 className="text-info">Peace be upon you</h1>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route path="/about" component={About}/>
+            <Route path="/project" component={Project}/>
+            <Route path="/services" component={Services}/>
+          </Switch>
+        </Router>
+        
+      </React.Fragment>
+    )
+  }
 }
+
 
 export default App;
